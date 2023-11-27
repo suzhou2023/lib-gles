cfa8f228cd9437850b6cb176244d30439f3ccdde
suzhou <withwind318@gmail.com>
2023-11-27 23:52:17 +0800

    misc

---
+++ src/main/cpp/3d/loadObj.cpp
@@ -0,0 +1,40 @@
+/**
+ *  author : suzhou
+ *  date : 2023/11/27 
+ *  description : 
+ */
+
+
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+
+using namespace glm;
+
+// 表示一个顶点属性
+struct Vertex {
+    vec3 position;  // 顶点位置
+    vec2 texCoords; // 纹理坐标
+    vec3 normal;  // 法向量
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
